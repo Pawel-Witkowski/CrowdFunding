3f95ceb4d270b6fb104dcee0c7b5c67feefbbf44
Pawel Witkowski <witkowski.pawel@hotmail.com>
2018-12-12 21:13:35 +0100

    initial commit

---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,79 @@
+pragma solidity ^0.4.25;
+
+contract CampaignFactory{
+    address[] public deployedCampaigns;
+    
+    function createCampaign(uint minimum) public{
+        deployedCampaigns.push(new Campaign(minimum, msg.sender));
+    }
+    
+    function getDeployedCampaigns() public view returns(address[]){
+        return deployedCampaigns;
+    }
+    
+}
+contract Campaign {
+    
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool completed;
+        mapping(address => bool) approvals;
+        uint approvalCount;
+    }
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribute;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+    
+    function Campaign(uint minimum, address campaignManager) public {
+        manager = campaignManager;
+        minimumContribute = minimum;
+        approversCount = 0;
+    }
+    
+    function contribute() public payable {
+        require(msg.value >= minimumContribute);
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+    
+    function createRequest(string description, uint value, address recipient) public restricted{
+        // require(approvers[msg.sender]);
+        Request memory req = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            completed: false,
+            approvalCount: 0
+        });
+        requests.push(req);
+    }
+    
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+        
+        request.approvalCount++;
+        request.approvals[msg.sender] = true;
+    }
+    
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+        require(request.approvalCount > (approversCount/2));
+        require(!request.completed);
+        
+        request.completed = true;
+        request.recipient.transfer(requests[index].value);
+        
+    }
+    
+    modifier restricted(){
+        require(msg.sender == manager);
+        _;
+    }
+    
+}
